95e607d0ee501071274b10bef709a626d9dd9dc6
Divya1234jain <divya.bhandari@habilelabs.io>
2023-10-30 19:23:15 +0530

    added controllers and error handlers

--- Route/Books.route.js
+++ Route/Books.route.js
@@ -1,57 +1,17 @@
 const express = require("express");
 const route = express.Router();
+const productController = require('../Controllers/book.controller');
 
 const Book = require("../Model/Book.model");
 
-route.get('/', async(req,res,next) => {
-    try {
-        const result = await Book.find({}, {__v:0})
-        res.send(result);
-    } catch (error) {
-        console.log(error.message);
-    }
-    
-})
+route.get('/', productController.getAllBooks );
 
-route.post('/', async (req,res,next) => {
-    try {
-        const book = new Book(req.body);
-        const result = await book.save();
-        res.send(result);
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+route.post('/', productController.createBook);
 
-route.get('/:id', async(req,res,next) => {
-    const id = req.params.id;
-    try {
-        const book = await Book.findById(id);
-        res.send(book);
-    } catch (error) {
-        console.log(error.message)
-    }
-})
+route.get('/:id', productController.getBookById);
 
-route.patch('/:id',async(req,res,next) => {
-    const id = req.params.id;
-    const updates = req.body;
-    try {
-        const result = await Book.findByIdAndUpdate(id,updates);
-        res.send(result);
-    } catch (error) {
-        console.log(error.message)
-    }
-})
+route.patch('/:id', productController.updateBookById);
 
-route.delete('/:id', async(req,res,next) => {
-    const id = req.params.id;
-    try {
-        const result = await Book.findByIdAndDelete(id);
-        res.send(result);
-    } catch (error) {
-        console.log(error.message)
-    }
-})
+route.delete('/:id', productController.deleteBookById);
 
 module.exports = route;
